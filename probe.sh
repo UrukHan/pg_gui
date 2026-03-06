ceaa004613034937fde538ca372b8f6857c62cad
a.urumov <urumov@easydev.group>
2026-03-06 22:37:39 +0300

    Remove temp probe scripts

--- probe.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TOKEN=$(curl -s -X POST http://localhost:8080/login -H 'Content-Type: application/json' -d '{"username":"admin","password":"admin"}' | python3 -c 'import sys,json; print(json.load(sys.stdin)["token"])')
-curl -s -H "Authorization: Bearer $TOKEN" http://localhost:8080/instruments/1/probe | python3 -m json.tool
